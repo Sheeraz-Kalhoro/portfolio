c869f45a9c1b2b9374cc9671b525d0a3d67c62dc
sheeraz-kalhoro <sheeraz@outlook.com>
2023-01-14 16:09:00 +0500

    lottie files added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import Education from './components/Education'
+import { Player } from '@lottiefiles/react-lottie-player';
 import Skills from './components/Skills';
 import Header from './components/Header'
 import Main from './components/Main';
